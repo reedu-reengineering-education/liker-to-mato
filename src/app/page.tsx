1de6f31082c75d83db0fe745ad06ec953f071c0e
Georgi-S <106542204+Georgi-S@users.noreply.github.com>
2023-08-23 12:42:12 +0200

    feat: add shadcn/ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Slider } from "@/components/ui/slider";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Input></Input>
+      <Card className="min-w-[20rem]">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
       <Button>Button</Button>
     </main>
-  )
+  );
 }
