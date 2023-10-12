1d7442f58c408b8c0dd6de2e1b8d830080d6ee10
Georgi-S <106542204+Georgi-S@users.noreply.github.com>
2023-10-12 13:20:05 +0200

    cleaning

--- src/app/studio/[surveyId]/page.tsx
+++ src/app/studio/[surveyId]/page.tsx
@@ -1,34 +1,9 @@
-import { Button } from "@/components/ui/button";
 import CreateQuestionDialog from "@/components/forms/create-question-form";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
 export default function Studio({ params }: { params: { surveyId: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hier</Button>
-
       <CreateQuestionDialog surveyId={params.surveyId} />
-      <Card className="min-w-[20rem]">
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-          <Button>Button</Button>
-          <Button>Button</Button>
-        </CardFooter>
-      </Card>
     </main>
   );
 }
