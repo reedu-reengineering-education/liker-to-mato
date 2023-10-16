bb34a9958ebf33afcb7e66bc515261decf0f2129
Georgi-S <106542204+Georgi-S@users.noreply.github.com>
2023-10-17 00:15:10 +0200

    add heading/descr. and the surve button prop

---
+++ src/app/about/page.tsx
@@ -0,0 +1,57 @@
+export function About() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>
+          Likert-to-Mat - Die perfekte App zur Umfrageerstellung für
+          Vorlesungsinhalte
+        </h1>
+      </div>
+      <p>
+        Likert-to-Mat ist die ideale Anwendung für Pädagogen, Dozenten und
+        Kursleiter, die hochwertige Umfragen zur Bewertung von
+        Vorlesungsinhalten erstellen möchten. Mit unserer benutzerfreundlichen
+        und leistungsstarken Plattform können Sie in wenigen Schritten
+        detaillierte Umfragen erstellen, um das Feedback und die Meinungen Ihrer
+        Studenten zu erfassen.
+      </p>
+      <ul>
+        <li>
+          Anpassbare Likert-Skalen: Erstellen Sie Umfragen, die auf der
+          Likert-Skala basieren, und passen Sie die Antwortmöglichkeiten an, um
+          genau die Informationen zu sammeln, die Sie benötigen.
+        </li>
+        <li>
+          Multimedia-Integration: Fügen Sie Bilder, Videos und andere Medien in
+          Ihre Umfragen ein, um den Lernprozess zu bereichern und präzises
+          Feedback zu erhalten.
+        </li>
+        <li>
+          Zeitgesteuerte Umfragen: Legen Sie Zeitpläne fest, um Umfragen
+          automatisch zu starten und zu beenden, und verfolgen Sie die
+          Ergebnisse in Echtzeit.
+        </li>
+        <li>
+          Analytik und Berichte: Nutzen Sie leistungsstarke Analysewerkzeuge, um
+          die Umfrageergebnisse zu verstehen und Muster in den Daten zu
+          erkennen.
+        </li>
+        <li>
+          Benutzerfreundliche Oberfläche: Unsere App wurde entwickelt, um die
+          Erstellung und Verwaltung von Umfragen so einfach wie möglich zu
+          gestalten, sodass Sie Zeit sparen und sich auf das Wesentliche
+          konzentrieren können.
+        </li>
+      </ul>
+      <p>
+        Mit Likert-to-Mat können Sie wertvolle Einblicke in die Lehr- und
+        Lernprozesse gewinnen, Ihre Vorlesungsinhalte kontinuierlich verbessern
+        und auf die Bedürfnisse Ihrer Studenten eingehen. Beginnen Sie noch
+        heute und optimieren Sie Ihre Unterrichtsmethoden mit präzisem Feedback
+        dank Likert-to-Mat.
+      </p>
+    </div>
+  );
+}
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+
 import { CreateSurveyDialog } from "@/components/forms/create-survey-form";
 
-export default function Home() {
+export function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CreateSurveyDialog />
     </main>
   );
 }
+export default Home;
