5a63823213c343e4ae42c68208a562ef2861ad9a
Georgi-S <106542204+Georgi-S@users.noreply.github.com>
2024-04-24 13:32:13 +0200

    fix a import path in the nextauth.ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 
